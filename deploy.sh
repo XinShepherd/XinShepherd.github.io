b231734cfe576415a1e5dd628abfc0a0c659b262
Fuxin <fuxin@dm-ai.cn>
2020-01-18 17:27:13 +0800

    Update index.html

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,14 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'example.com' > CNAME
 
+git config --global user.email "okfuxin@gmail.com"
+git config --global user.name "Fuxin"
 echo Deploying..
 git init
 git add -A
 git commit -m 'deploy'
 
 # deploy
-git push -f https://github.com/XinShepherd/XinShepherd.github.io.git master
+git push -f git@github.com:XinShepherd/XinShepherd.github.io.git master
 
 cd -
